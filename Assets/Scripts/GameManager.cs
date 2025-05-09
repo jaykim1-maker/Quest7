1ba6c31b07dbadac29712c5ab01c2ac003db22c2
myosotis0423 <shellingford52@gmail.com>
2025-05-09 18:52:45 +0900

    게임오버, 재시작

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,4 +29,15 @@ public class GameManager : MonoBehaviour
 
     #endregion  
 
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//게임이 재시작되면 시간을 초기화하고 처음화면을 불러온다
+    }
+
+    public void LoadMain()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main"); // 메인 메뉴 씬 이름에 맞게 수정
+    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,19 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject gameOverPanel;
+    public Text scoreText;
+    public Text bestScoreText;
+    public GameManager gameManager;
+    public Button restartButton;
+
     public TextMeshProUGUI score;
+
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+        Time.timeScale = 1f;
+
+        if (restartButton != null)
+        {
+            restartButton.onClick.AddListener(RestartGame);
+        }
+
+    }
+
+    public void CheckPlayerHP(int hp)
+    {
+        if (hp <= 0)
+        {
+            ShowGameOver();
+        }
+    }
+
+    public void ShowGameOver()
+    {
+        isGameOver = true;
+        gameOverPanel.SetActive(true);
+
+        int score = gameManager.score;
+        int bestScore = PlayerPrefs.GetInt(BestScore, 0);
+
+        if(score > bestScore)
+        {
+            bestScore = score;
+            PlayerPrefs.SetInt("BestScore", bestScore);
+        }
+
+
+    }
 
     public void UpdateUI()
     {
-
+        gameOverPanel.SetActive(false);
+        Time.timeScale = 0f;
     }
 
     public void UpdateScore()
     {
 
     }
+
+     void Update()
+    {
+ 
+    }
+
+
+
+    void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
