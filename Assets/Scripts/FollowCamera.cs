1b66192ad4e5323da377c9665c42c3bd54ed7474
Siznon <rlawndska1004@gmail.com>
2025-05-09 20:48:38 +0900

    최대속도 고정

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    public float followSpeed = 0.02f;         // 시작 속도
-    public float speedMultiplier = 1.02f;     // 몇 배로 증가할지
-    public float interval = 5f;              // 몇 초마다 증가할지
-    public float maxSpeed = 1.0f;
+    public float followSpeed = 0.03f;         // 시작 속도
+    public float speedMultiplier = 1.01f;     // 몇 배로 증가할지
+    public float interval = 10f;              // 몇 초마다 증가할지
+    public float maxSpeed = 1.0f;             //최대 속도
     private float timer = 0f;                // 시간 누적용
 
     void Update()
