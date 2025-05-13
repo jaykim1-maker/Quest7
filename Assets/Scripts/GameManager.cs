f6ffb8c751bfc7a8d223b12693ce0bf237aa1853
Siznon <rlawndska1004@gmail.com>
2025-05-13 20:54:43 +0900

    1
    1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     [SerializeField] public UIManager uimanager;
-    private UIManager uiManager;
-    public static bool isFirstLoading = true;
+    [SerializeField] public FollowCamera followCamera;
 
     public int score = 0;
 
@@ -18,25 +17,8 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
-        uiManager = FindObjectOfType<UIManager>();
     }
 
-    private void Start()
-    {
-        if (!isFirstLoading)
-        {
-            StartGame();
-        }
-        else
-        {
-            isFirstLoading = false;
-        }
-    }
-
-    public void StartGame()
-    {
-        uiManager.SetPlayGame();
-    }
     #region UIManager
     public void UpdateUI()
     {
@@ -47,18 +29,32 @@ public class GameManager : MonoBehaviour
     {
         uimanager.UpdateScore();
     }
-
     #endregion  
+
+    // 게임 오버 시 카메라 초기화
+    public void OnGameOver()
+    {
+        if (followCamera != null)
+            followCamera.ResetCameraOnGameOver(); // 카메라 초기화
+
+        // 게임 오버 처리 추가 (예: UI 표시, 일시정지 등)
+        Debug.Log("게임 오버! 카메라 위치 초기화 완료");
+    }
 
     public void RestartGame()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//게임이 재시작되면 시간을 초기화하고 처음화면을 불러온다
-    }
+        if (followCamera != null)
+            followCamera.ResetCameraOnGameOver(); // 카메라 초기화
 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
     public void LoadMain()
     {
         Time.timeScale = 1f;
+        if (followCamera != null)
+            followCamera.ResetCameraOnGameOver(); // 카메라 초기화
+
         SceneManager.LoadScene("Main"); // 메인 메뉴 씬 이름에 맞게 수정
     }
 }
