0e9d45553eabfb875593227b41eddd8da8ecd118
Kim-Jay <wodml0123@naver.com>
2025-05-14 17:36:14 +0900

    Player 중간저장
    85% 완료

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,10 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     [SerializeField] public UIManager uimanager;
-    private Player player;
     private UIManager uiManager;
     public static bool isFirstLoading = true;
 
     public int score = 0;
-    public int HP = 100;
-
 
     private void Awake()
     {
@@ -38,7 +35,7 @@ public class GameManager : MonoBehaviour
 
     public void StartGame()
     {
-        score = 0;
+        Time.timeScale = 1f;
         uiManager.SetPlayGame();
     }
     #region UIManager
@@ -56,7 +53,6 @@ public class GameManager : MonoBehaviour
 
     public void RestartGame()
     {
-        score = 0;
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//게임이 재시작되면 시간을 초기화하고 처음화면을 불러온다
     }
@@ -65,14 +61,5 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Main"); // 메인 메뉴 씬 이름에 맞게 수정
-    }
-
-    public void AddCoin()
-    {
-        score++;
-        HP += 5;
-        if (HP >= 100)
-            { HP = 100; }
-        Debug.Log(score);
     }
 }
